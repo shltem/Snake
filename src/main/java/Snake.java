ca473e40d5e993e0461b44738696fd1ffef7c74f
shlomo.templeman <shltem@gmail.com>
2023-05-03 18:02:21 +0300

    init

--- src/main/java/Snake.java
+++ src/main/java/Snake.java
@@ -1,15 +1,20 @@
 import java.util.ArrayList;
 import java.util.List;
 public class Snake {
+
     private final List<Position> snakeBody = new ArrayList<>();
+
     private Direction direction;
+
     private Snake() {
         direction = Direction.RIGHT;
         snakeBody.add(new Position(1,1)); // up left position to start the game
     }
+
     public void setDirection(Direction newDirection) {
         direction = newDirection;
     }
+
     public void move() {
         Position head = getHead();
         Position newHead;
@@ -37,15 +42,19 @@ public class Snake {
             snakeBody.remove(snakeBody.size() - 1); // remove tail
         }
     }
+
     private Position getHead() {
         return snakeBody.get(0);
     }
+
     public List<Position> getSnakePositions() {
         return snakeBody;
     }
+
     public boolean isCollision(){
         return isSelfCollision() || isBorderCollision();
     }
+
     private boolean isSelfCollision() {
             for(int i = 1; i < snakeBody.size(); ++i){
                 if(snakeBody.get(i).equals(snakeBody.get(0))){
@@ -54,16 +63,20 @@ public class Snake {
             }
         return false;
     }
+
     private boolean isBorderCollision() {
         return getHead().getX() <= 0 || getHead().getX() >= Border.getInstance().getWidth() -1||
                 getHead().getY() <= 0 || getHead().getY() >= Border.getInstance().getHeight() -1;
     }
+
     private static class SingletonHolder {
         private static final Snake INSTANCE = new Snake();
     }
+
     public static Snake getInstance() {
         return Snake.SingletonHolder.INSTANCE;
     }
+
     public Direction getDirection() {
         return direction;
     }
